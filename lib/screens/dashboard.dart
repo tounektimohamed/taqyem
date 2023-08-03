14500f8238047da150a0df786ddb635b74cb320d
Ramith Gunawardana <Ramith-Gunawardana@users.noreply.github.com>
2023-08-03 18:04:00 +0530

    Adding transparent app icon

--- lib/screens/dashboard.dart
+++ lib/screens/dashboard.dart
@@ -1,3 +1,4 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 
@@ -10,6 +11,27 @@ class Dashboard extends StatefulWidget {
 
 class _DashboardState extends State<Dashboard> {
   final user = FirebaseAuth.instance.currentUser;
+
+  //documnet IDs
+  List<String> docIDs = [];
+
+  //get docIDs
+  Future getDocIDs() async {
+    await FirebaseFirestore.instance.collection('users').get().then(
+          (snapshot) => snapshot.docs.forEach(
+            (documnet) {
+              print(documnet.reference);
+              docIDs.add(documnet.reference.id);
+            },
+          ),
+        );
+  }
+
+  @override
+  void initState() {
+    getDocIDs();
+    super.initState();
+  }
 
   @override
   Widget build(BuildContext context) {
