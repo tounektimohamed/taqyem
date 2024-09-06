d198495918dbf4ec36f50d12521d1175d7169690
tounektimohamed <87899355+tounektimohamed@users.noreply.github.com>
2024-09-06 17:04:51 +0100

    Update sigweb.dart

--- lib/screens2/jeojson/sigweb.dart
+++ lib/screens2/jeojson/sigweb.dart
@@ -2,11 +2,13 @@ import 'dart:convert';
 import 'dart:typed_data';
 import 'package:DREHATT_app/screens2/jeojson/convertGeoJson.dart';
 import 'package:DREHATT_app/screens2/kml/KmlMapPage.dart';
+import 'package:firebase_storage/firebase_storage.dart';
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
 import 'package:file_picker/file_picker.dart';
+import 'package:http/http.dart' as http;
 
 class SigWeb extends StatefulWidget {
   const SigWeb({super.key, required this.title});
@@ -19,6 +21,8 @@ class SigWeb extends StatefulWidget {
 
 class _SigWebState extends State<SigWeb> {
   bool loadingData = false;
+  bool uploadingData = false;
+  double uploadProgress = 0.0; // Add this line
   GoogleMapController? mapController;
   String _selectedGeoJsonDocumentId = '';
   Set<Polygon> polygons = {};
@@ -60,69 +64,107 @@ class _SigWebState extends State<SigWeb> {
     );
   }
 
-Future<void> loadGeoJsonFromFirestore(String documentId) async {
-  if (mapController == null) {
-    print('Map controller is not initialized');
-    return;
+  Future<void> uploadGeoJsonToStorage(
+      String fileName, Uint8List fileBytes) async {
+    try {
+      final storageRef =
+          FirebaseStorage.instance.ref().child('geojson_files/$fileName');
+      await storageRef.putData(fileBytes);
+      final downloadUrl = await storageRef.getDownloadURL();
+
+      print('GeoJSON uploaded to Storage successfully: $downloadUrl');
+    } catch (e) {
+      print('Error uploading GeoJSON: $e');
+    }
   }
 
-  try {
-    setState(() {
-      loadingData = true;
-    });
+  Future<void> loadGeoJsonFromStorage(String fileName) async {
+    if (mapController == null) {
+      print('Map controller is not initialized');
+      return;
+    }
 
-    final firestore = FirebaseFirestore.instance;
-    final docSnapshot = await firestore.collection('geojson_files').doc(documentId).get();
+    try {
+      setState(() {
+        loadingData = true;
+      });
 
-    if (docSnapshot.exists) {
-      final geoJsonData = docSnapshot.data()?['geojson'] as String?;
-      if (geoJsonData != null) {
-        final decodedGeoJson = json.decode(geoJsonData);
-        final Set<Polygon> newPolygons = {};
-        final Set<Polyline> newPolylines = {}; 
+      final storageRef =
+          FirebaseStorage.instance.ref().child('geojson_files/$fileName');
+      final downloadUrl = await storageRef.getDownloadURL();
+      final response = await http.get(Uri.parse(downloadUrl));
+      final geoJsonData = response.body;
 
-        int index = 0; // Initialize index for unique ID
-        for (var feature in decodedGeoJson['features']) {
-          if (feature['geometry']['type'] == 'MultiPolygon') {
-            // Handle MultiPolygon
-            final List<List<LatLng>> polygonList = [];
-            for (var polygon in feature['geometry']['coordinates']) {
-              final List<LatLng> ringPoints = [];
-              for (var ring in polygon) {
-                for (var coord in ring) {
-                  ringPoints.add(LatLng(coord[1], coord[0]));
-                }
+      final decodedGeoJson = json.decode(geoJsonData);
+      final Set<Polygon> newPolygons = {};
+      final Set<Polyline> newPolylines = {};
+
+      int index = 0; // Initialize index for unique ID
+      for (var feature in decodedGeoJson['features']) {
+        if (feature['geometry']['type'] == 'MultiPolygon') {
+          // Handle MultiPolygon
+          final List<List<LatLng>> polygonList = [];
+          for (var polygon in feature['geometry']['coordinates']) {
+            final List<LatLng> ringPoints = [];
+            for (var ring in polygon) {
+              for (var coord in ring) {
+                ringPoints.add(LatLng(coord[1], coord[0]));
               }
-              polygonList.add(ringPoints);
             }
+            polygonList.add(ringPoints);
+          }
 
-            final polygonId = PolygonId('polygon_$index');
-            index++;
+          final polygonId = PolygonId('polygon_$index');
+          index++;
 
-            final String? fillHex = feature['properties']['fill'];
-            final double fillOpacity = feature['properties']['fill-opacity']?.toDouble() ?? 0.5;
-            final Color fillColor = fillHex != null
-                ? Color(int.parse(fillHex.replaceFirst('#', '0xFF')))
-                : Color.fromARGB(0, 236, 125, 125);
+          final String? fillHex = feature['properties']['fill'];
+          final double fillOpacity =
+              feature['properties']['fill-opacity']?.toDouble() ?? 0.5;
+          final Color fillColor = fillHex != null
+              ? Color(int.parse(fillHex.replaceFirst('#', '0xFF')))
+              : Color.fromARGB(0, 236, 125, 125);
 
-            newPolygons.add(
-              Polygon(
-                polygonId: polygonId,
-                points: polygonList.expand((ring) => ring).toList(),
-                strokeColor: feature['properties']['stroke'] != null
-                    ? Color(int.parse(feature['properties']['stroke'].replaceFirst('#', '0xFF')))
-                    : Colors.black,
-                strokeWidth: feature['properties']['stroke-width']?.toDouble() ?? 2.0,
-                fillColor: fillColor.withOpacity(fillOpacity),
-                onTap: () {
-                  _showPolygonInfo(feature['properties']);
-                },
-              ),
-            );
-          } else if (feature['geometry']['type'] == 'LineString') {
-            // Handle LineString
+          newPolygons.add(
+            Polygon(
+              polygonId: polygonId,
+              points: polygonList.expand((ring) => ring).toList(),
+              strokeColor: feature['properties']['stroke'] != null
+                  ? Color(int.parse(feature['properties']['stroke']
+                      .replaceFirst('#', '0xFF')))
+                  : Colors.black,
+              strokeWidth:
+                  feature['properties']['stroke-width']?.toDouble() ?? 2.0,
+              fillColor: fillColor.withOpacity(fillOpacity),
+              onTap: () {
+                _showPolygonInfo(feature['properties']);
+              },
+            ),
+          );
+        } else if (feature['geometry']['type'] == 'LineString') {
+          // Handle LineString
+          final List<LatLng> linePoints = [];
+          for (var coord in feature['geometry']['coordinates']) {
+            if (coord is List && coord.length >= 2) {
+              linePoints.add(LatLng(coord[1], coord[0]));
+            }
+          }
+
+          final polylineId = PolylineId('polyline_$index');
+          index++;
+
+          newPolylines.add(
+            Polyline(
+              polylineId: polylineId,
+              points: linePoints,
+              color: Colors.red,
+              width: 3,
+            ),
+          );
+        } else if (feature['geometry']['type'] == 'MultiLineString') {
+          // Handle MultiLineString
+          for (var line in feature['geometry']['coordinates']) {
             final List<LatLng> linePoints = [];
-            for (var coord in feature['geometry']['coordinates']) {
+            for (var coord in line) {
               if (coord is List && coord.length >= 2) {
                 linePoints.add(LatLng(coord[1], coord[0]));
               }
@@ -135,63 +177,37 @@ Future<void> loadGeoJsonFromFirestore(String documentId) async {
               Polyline(
                 polylineId: polylineId,
                 points: linePoints,
-                color: Colors.red,
+                color: Color.fromARGB(
+                    255, 255, 179, 66), // Default color for MultiLineString
                 width: 3,
               ),
             );
-          } else if (feature['geometry']['type'] == 'MultiLineString') {
-            // Handle MultiLineString
-            for (var line in feature['geometry']['coordinates']) {
-              final List<LatLng> linePoints = [];
-              for (var coord in line) {
-                if (coord is List && coord.length >= 2) {
-                  linePoints.add(LatLng(coord[1], coord[0]));
-                }
-              }
-
-              final polylineId = PolylineId('polyline_$index');
-              index++;
-
-              newPolylines.add(
-                Polyline(
-                  polylineId: polylineId,
-                  points: linePoints,
-                  color: Colors.blue, // Default color for MultiLineString
-                  width: 3,
-                ),
-              );
-            }
           }
         }
-
-        setState(() {
-          polygons = newPolygons;
-          polylines = newPolylines;
-        });
-
-        if (newPolygons.isNotEmpty || newPolylines.isNotEmpty) {
-          final allPoints = newPolygons.expand((polygon) => polygon.points).toList();
-          allPoints.addAll(newPolylines.expand((polyline) => polyline.points));
-          final bounds = calculateBoundingBox(allPoints);
-          mapController!.animateCamera(CameraUpdate.newLatLngBounds(bounds, 50));
-        }
-
-        print('GeoJSON loaded and parsed successfully');
-      } else {
-        print('Field "geojson" does not exist in the document');
       }
-    } else {
-      print('Document does not exist');
-    }
-  } catch (e) {
-    print('Error loading GeoJSON: $e');
-  } finally {
-    setState(() {
-      loadingData = false;
-    });
-  }
-}
 
+      setState(() {
+        polygons = newPolygons;
+        polylines = newPolylines;
+      });
+
+      if (newPolygons.isNotEmpty || newPolylines.isNotEmpty) {
+        final allPoints =
+            newPolygons.expand((polygon) => polygon.points).toList();
+        allPoints.addAll(newPolylines.expand((polyline) => polyline.points));
+        final bounds = calculateBoundingBox(allPoints);
+        mapController!.animateCamera(CameraUpdate.newLatLngBounds(bounds, 50));
+      }
+
+      print('GeoJSON loaded and parsed successfully');
+    } catch (e) {
+      print('Error loading GeoJSON: $e');
+    } finally {
+      setState(() {
+        loadingData = false;
+      });
+    }
+  }
 
   Future<void> uploadGeoJsonToFirestore(
       String documentId, String geoJsonData) async {
@@ -206,42 +222,108 @@ Future<void> loadGeoJsonFromFirestore(String documentId) async {
     }
   }
 
-  Future<void> _showGeoJsonSelectionDialog() async {
-    final firestore = FirebaseFirestore.instance;
-    final querySnapshot = await firestore.collection('geojson_files').get();
+  Future<void> _deleteGeoJsonFile(String fileName) async {
+    try {
+      final storageRef =
+          FirebaseStorage.instance.ref().child('geojson_files/$fileName');
+      await storageRef.delete();
+      print('File deleted successfully');
 
-    final documents = querySnapshot.docs;
+      // Refresh the list of GeoJSON files after deletion
+      _showGeoJsonSelectionDialog();
+    } catch (e) {
+      print('Error deleting file: $e');
+    }
+  }
 
-    showDialog(
+  Future<void> _confirmAndDeleteGeoJsonFile(String fileName) async {
+    // Show a confirmation dialog before deleting the file
+    final confirmed = await showDialog<bool>(
       context: context,
       builder: (BuildContext context) {
         return AlertDialog(
-          title: Text('Select a GeoJSON File'),
-          content: Container(
-            width: double.maxFinite,
-            child: ListView(
-              children: documents.map((doc) {
-                final documentId = doc.id;
-                final fileName = documentId;
-
-                return ListTile(
-                  title: Text(fileName),
-                  onTap: () {
-                    Navigator.of(context).pop(); // Close the dialog
-                    _selectGeoJson(documentId);
-                  },
-                );
-              }).toList(),
+          title: Text('Confirm Deletion'),
+          content: Text('Are you sure you want to delete this file?'),
+          actions: [
+            TextButton(
+              onPressed: () => Navigator.of(context).pop(false),
+              child: Text('No'),
             ),
-          ),
+            TextButton(
+              onPressed: () => Navigator.of(context).pop(true),
+              child: Text('Yes'),
+            ),
+          ],
         );
       },
     );
+
+    if (confirmed == true) {
+      // Proceed with deletion if confirmed
+      try {
+        final storageRef =
+            FirebaseStorage.instance.ref().child('geojson_files/$fileName');
+        await storageRef.delete();
+        print('File deleted successfully');
+
+        // Refresh the list of GeoJSON files after deletion
+        _showGeoJsonSelectionDialog();
+      } catch (e) {
+        print('Error deleting file: $e');
+      }
+    }
   }
 
-  Future<void> _selectGeoJson(String documentId) async {
+  Future<void> _showGeoJsonSelectionDialog() async {
     try {
-      await loadGeoJsonFromFirestore(documentId);
+      final storage = FirebaseStorage.instance;
+      final listResult = await storage.ref('geojson_files').listAll();
+      print(
+          'Files in Storage: ${listResult.items.length}'); // Debug: print number of items
+
+      showDialog(
+        context: context,
+        builder: (BuildContext context) {
+          return AlertDialog(
+            title: Text('Select a GeoJSON File'),
+            content: Container(
+              width: double.maxFinite,
+              child: ListView(
+                children: listResult.items.map((fileRef) {
+                  final fileName = fileRef.name;
+
+                  return ListTile(
+                    title: Text(fileName),
+                    onTap: () {
+                      Navigator.of(context).pop(); // Close the dialog
+                      _selectGeoJson(fileName);
+                    },
+                    trailing: IconButton(
+                      icon: Icon(Icons.delete, color: Colors.red),
+                      onPressed: () {
+                        Navigator.of(context).pop(); // Close the dialog
+                        _confirmAndDeleteGeoJsonFile(fileName);
+                      },
+                    ),
+                  );
+                }).toList(),
+              ),
+            ),
+          );
+        },
+      );
+    } catch (e) {
+      print('Error listing GeoJSON files: $e'); // Show detailed error
+      if (e is FirebaseException) {
+        print('Error Code: ${e.code}');
+        print('Error Message: ${e.message}');
+      }
+    }
+  }
+
+  Future<void> _selectGeoJson(String fileName) async {
+    try {
+      await loadGeoJsonFromStorage(fileName);
     } catch (e) {
       print('Error selecting GeoJSON file: $e');
     }
@@ -249,23 +331,48 @@ Future<void> loadGeoJsonFromFirestore(String documentId) async {
 
   Future<void> _uploadGeoJsonFile() async {
     try {
+      setState(() {
+        uploadingData = true;
+        uploadProgress = 0.0;
+      });
+
       final result = await FilePicker.platform
           .pickFiles(type: FileType.custom, allowedExtensions: ['geojson']);
       if (result != null && result.files.isNotEmpty) {
         final file = result.files.first;
 
         final Uint8List fileBytes = file.bytes!;
-        final geoJsonData = String.fromCharCodes(fileBytes);
-        final fileName = file.name;
+        final String fileName = file.name;
 
-        await uploadGeoJsonToFirestore(fileName, geoJsonData);
+        final storageRef =
+            FirebaseStorage.instance.ref().child('geojson_files/$fileName');
+        final uploadTask = storageRef.putData(fileBytes);
 
-        print('File uploaded successfully: $fileName');
-      } else {
-        print('No file selected');
+        uploadTask.snapshotEvents.listen((taskSnapshot) {
+          setState(() {
+            uploadProgress =
+                (taskSnapshot.bytesTransferred / taskSnapshot.totalBytes) * 100;
+          });
+        });
+
+        await uploadTask.whenComplete(() async {
+          final downloadUrl = await storageRef.getDownloadURL();
+          print('File uploaded successfully: $downloadUrl');
+          setState(() {
+            uploadingData = false;
+          });
+        }).catchError((error) {
+          print('Error uploading file: $error');
+          setState(() {
+            uploadingData = false;
+          });
+        });
       }
     } catch (e) {
-      print('Error uploading file: $e');
+      print('Error picking file: $e');
+      setState(() {
+        uploadingData = false;
+      });
     }
   }
 
@@ -438,6 +545,29 @@ Future<void> loadGeoJsonFromFirestore(String documentId) async {
           if (loadingData)
             Center(
               child: CircularProgressIndicator(),
+            ),
+          if (uploadingData) // Show the upload indicator with percentage
+            Center(
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  CircularProgressIndicator(value: uploadProgress / 100),
+                  SizedBox(height: 20),
+                  Container(
+                    padding:
+                        EdgeInsets.symmetric(horizontal: 16.0, vertical: 8.0),
+                    color: Colors.black
+                        .withOpacity(0.7), // Background color with opacity
+                    child: Text(
+                      '${uploadProgress.toStringAsFixed(1)}%',
+                      style: TextStyle(
+                        color: Colors.white, // Text color
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                  ),
+                ],
+              ),
             ),
         ],
       ),
