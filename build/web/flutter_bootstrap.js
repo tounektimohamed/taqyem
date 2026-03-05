13cd71c3c73461ba7fe540f495b46684a1192f9e
tounektimohamed <tounekti17@gmail.com>
2026-03-05 03:07:27 +0100

    res

--- build/web/flutter_bootstrap.js
+++ build/web/flutter_bootstrap.js
@@ -38,6 +38,6 @@ _flutter.buildConfig = {"engineRevision":"ddf47dd3ff96dbde6d9c614db0d7f019d7c7a2
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "2963349306"
+    serviceWorkerVersion: "1122927633"
   }
 });
